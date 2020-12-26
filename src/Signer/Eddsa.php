c334f63f1875a7ddf8611ebe07141ac8f8a8fc56
deatil <2217957370@qq.com>
2020-12-26 13:45:12 +0800

    修复eddsa不能用问题

--- src/Signer/Eddsa.php
+++ src/Signer/Eddsa.php
@@ -5,7 +5,6 @@ namespace Larke\JWT\Signer;
 use SodiumException;
 
 use Larke\JWT\Exception\InvalidKeyProvided;
-use Larke\JWT\Contracts\Signer;
 use Larke\JWT\Contracts\Key;
 
 use function sodium_crypto_sign_detached;
@@ -14,26 +13,26 @@ use function sodium_crypto_sign_verify_detached;
 /**
  * EDDSA signers
  */
-final class Eddsa implements Signer
+class Eddsa extends BaseSigner
 {
-    public function algorithmId()
+    public function getAlgorithmId()
     {
         return 'EdDSA';
     }
-
-    public function sign(string $payload, Key $key)
+    
+    public function createHash($payload, Key $key)
     {
         try {
-            return sodium_crypto_sign_detached($payload, $key->contents());
+            return sodium_crypto_sign_detached($payload, $key->getContent());
         } catch (SodiumException $sodiumException) {
             throw new InvalidKeyProvided($sodiumException->getMessage(), 0, $sodiumException);
         }
     }
 
-    public function verify(string $expected, string $payload, Key $key)
+    public function doVerify($expected, $payload, Key $key)
     {
         try {
-            return sodium_crypto_sign_verify_detached($expected, $payload, $key->contents());
+            return sodium_crypto_sign_verify_detached($expected, $payload, $key->getContent());
         } catch (SodiumException $sodiumException) {
             throw new InvalidKeyProvided($sodiumException->getMessage(), 0, $sodiumException);
         }
